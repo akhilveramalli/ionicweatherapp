8fd26f2d69d40dcca48e1500f1f04e193d0973b0
akhilveramalli <veeramalliakhil@gmail.com>
2024-02-06 12:29:07 +0530

    added upi payment qr code creation tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [ BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [ BrowserModule, HttpClientModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BarcodeScanner,],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
+import { Storage } from '@ionic/storage-angular';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,103 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  private _storage: Storage | null = null;
 
-  constructor() {}
+  scannedData: any;
+  encodedData: any;
+  encodeData: any;
+  inputData: any;
+  number : any;
+  
+
+  constructor(private barcodeScanner: BarcodeScanner,private storage: Storage,private alertController: AlertController) {this.init();}
+
+  scanBarcode() {
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Place a barcode inside the scan area',
+      resultDisplayDuration: 500,
+      formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+      orientation: 'portrait',
+    };
+
+    this.barcodeScanner.scan(options).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.scannedData = barcodeData;
+
+    }).catch(err => {
+      console.log('Error', err);
+    });
+
+  }
+
+  createBarcode() {
+    var data = 'upi://pay?pa='+this.number+'&pn=vdfvgdvg&am='+this.inputData+'&cu=INR&tn=f&' ;
+    this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, data).then((encodedData) => {
+      console.log(encodedData);
+      this.encodedData = encodedData;
+    }, (err) => {
+      console.log('Error occured : ' + err);
+    });
+  }
+
+  async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
+    const storage = await this.storage.create();
+    this._storage = storage;
+    this.number = await storage.get('name');
+    console.log(this.number)
+    if (this.number == null){
+      await this.showEnterNameAlert();
+    }
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  async showEnterNameAlert() {
+    const alert = await this.alertController.create({
+        header: 'Enter Name',
+        message: 'Please enter your UPI ID:',
+        inputs: [
+            {
+                name: 'name',
+                type: 'text',
+                placeholder: 'UPI ID'
+            }
+        ],
+        buttons: [
+            {
+                text: 'Cancel',
+                role: 'cancel',
+                handler: () => {
+                    console.log('Cancel clicked');
+                }
+            },
+            {
+                text: 'Submit',
+                handler: (data) => {
+                    // Handle the entered name (data.name)
+                    console.log('Entered Name:', data.name);
+                    this.set('name', data.name)
+                    this.init()
+                }
+            }
+        ]
+    });
+
+    await alert.present();  
+} 
+
+
+
+
+
 
 }
